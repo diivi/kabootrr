3d7d91ce98b168becad7e0d7a2923ad96e97dd9d
Manavvgarg 3 <manavvgarg2019@gmail.com>
2020-05-25 23:33:51 +0530

    New

--- fbinit.js
+++ fbinit.js
@@ -10,7 +10,3 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
-<<<<<<< HEAD
-=======
-//divyanshanandsingh@gmail.com
->>>>>>> 3764dc4cc46198776012a61665f29e097ea1dbe7
